b239565224203e8a4b66f43d925c0139300c6c87
JuneCho23 <junecho.ucb@gmail.com>
2017-03-17 15:10:17 -0700

    ViewModel

--- BarcodeTestApp/BaseViewModel.cs
+++ BarcodeTestApp/BaseViewModel.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace BarcodeTestApp
 {
-	public class BaseViewModel : INotifyPropertyChanged 
+	public class BaseViewModel : INotifyPropertyChanged
 	//I = Capital I defines a interface
 	{
 		public BaseViewModel()
 		{
 		}
+
+		ImageSource localSource = "icon.png";
+		string path = "";
+		ICommand takePhotoCommand;
+		//Capital I = Interface !!
+
+		public ImageSource LocalImageSource
+		{
+			get { return localSource; }
+			set
+			{
+				localSource = value;
+				OnPropertyChanged();
+			}
+		}
+
+
+
+		public async Task TakePhoto()
+		{
+
+		}
+
+
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
