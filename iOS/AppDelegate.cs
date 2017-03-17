882301303a5722777d725d1c6dee991632e9e1f1
JuneCho23 <junecho.ucb@gmail.com>
2017-03-17 13:09:09 -0700

    Update AppDelegate

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -13,6 +13,7 @@ namespace BarcodeTestApp.iOS
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
+			ZXing.Net.Mobile.Forms.iOS.Platform.Init();
 
 			LoadApplication(new App());
 
