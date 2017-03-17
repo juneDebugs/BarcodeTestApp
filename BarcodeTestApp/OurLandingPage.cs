880ba5611fa17d972fd4dac67f2d6e6e248ccd3e
JuneCho23 <junecho.ucb@gmail.com>
2017-03-17 14:06:19 -0700

    ViewModel

--- BarcodeTestApp/OurLandingPage.cs
+++ BarcodeTestApp/OurLandingPage.cs
@@ -8,9 +8,21 @@ namespace BarcodeTestApp
 		{
 			var image = new Image();
 			var pathLabel = new Label();
-			var button = new Button { Text = "Take Picture" };
+			var button = new Button { Text = "Take Picture", };
 			//curly brakets define inline with the constructor
-				
+			//CTRL + SpaceBar shoes all properties 
+
+			Content = new StackLayout
+			{
+				Children = 
+				{
+					image,
+					pathLabel,
+					button
+				}
+			};
+
+
 		}
 	}
 }
---
+++ BarcodeTestApp/OurLandingPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel;
+namespace BarcodeTestApp
+{
+	public class OurLandingPageViewModel : INotifyPropertyChanged 
+	//I = Capital I defines a interface
+	{
+		public OurLandingPageViewModel()
+		{
+		}
+	}
+}
