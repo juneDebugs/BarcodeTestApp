ae11c3af963c4722690623efe0eec2071b675479
JuneCho23 <junecho.ucb@gmail.com>
2017-03-17 13:05:13 -0700

    Update Android MainActivity

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -21,8 +21,14 @@ namespace BarcodeTestApp.Droid
 			base.OnCreate(bundle);
 
 			global::Xamarin.Forms.Forms.Init(this, bundle);
+			ZXing.Net.Mobile.Forms.Android.Platform.Init();
 
 			LoadApplication(new App());
+		}
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+		{
+			global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 		}
 	}
 }
